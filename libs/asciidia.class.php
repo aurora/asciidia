132eb1079a317c0250be87c9b627c6f30b8dceca
Harald Lapp <harald.lapp@gmail.com>
2011-07-11 19:40:32 +0200

    - added license information

--- libs/asciidia.class.php
+++ libs/asciidia.class.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+ * This file is part of asciidia
+ * Copyright (C) 2011 by Harald Lapp <harald@octris.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This script can be found at:
+ * https://github.com/aurora/asciidia
+ */
+
 /**
  * ASCII Diagram core class. Implements text parser and core functionality of
  * text-diagram to imagemagick-draw converter.
