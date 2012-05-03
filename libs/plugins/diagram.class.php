ae700fb58daf5b08757179e10e283e89d0339b58
Harald Lapp <harald.lapp@gmail.com>
2012-05-03 18:02:26 +0200

    removed obsolete method

--- libs/plugins/diagram.class.php
+++ libs/plugins/diagram.class.php
@@ -82,16 +82,6 @@ class diagram extends plugin
     /**/
     
     /**
-     * Display usage information.
-     *
-     * @octdoc  m:diagram/usage
-     */
-    public function usage()
-    /**/
-    {
-    }
-    
-    /**
      * Add character to list of strings to determine text-strings to render.
      *
      * @octdoc  m:diagram/addChar
