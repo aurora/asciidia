91c498362ecc6109900b90f1a682dfd3e5168a84
Harald Lapp <h.lapp@clipdealer.de>
2014-03-10 11:19:37 +0100

    made '{' '}' at start/end of EBNF optional

--- libs/plugins/ebnf.class.php
+++ libs/plugins/ebnf.class.php
@@ -584,17 +584,17 @@ namespace asciidia\plugins {
             $dom    = new \DOMDocument();
             $syntax = $dom->appendChild($dom->createElement('syntax'));
         
-            // if (!($token = $this->getToken($tokens, self::T_OPERATOR, '{'))) {
-            //     // $this->error('EBNF must start with "{"');
-            // }
+            if (($token = $this->getToken($tokens, self::T_OPERATOR, '{', false))) {
+                $this->eatToken($tokens);
+            }
 
             while (count($tokens) > 0 && ($token = $this->getToken($tokens, self::T_IDENTIFIER))) {
                 $syntax->appendChild($this->parseProd($dom, $tokens, $token));
             }
         
-            // if (count($tokens) > 1 || $this->chkToken($token, self::T_OPERATOR, '}')) {
-            //     // $this->error('EBNF must end with "}"');
-            // }
+            if (count($tokens) > 1 || (count($tokens) == 1 && !$this->chkToken($token, self::T_OPERATOR, '}'))) {
+                $this->error('unexpected end of EBNF');
+            }
 
             // render syntax and return it's MVG commands
             $this->render($syntax);
